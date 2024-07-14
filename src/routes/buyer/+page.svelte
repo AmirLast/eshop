ae3e02ad4eb7a998c41b5e7abf5df12cf7d4eb3f
muhammedmunir <mmunir6@graduate.utm.my>
2024-07-14 21:20:29 +0800

    update

--- src/routes/buyer/+page.svelte
+++ src/routes/buyer/+page.svelte
@@ -21,11 +21,9 @@
 <p class="text-lg mb-6">Times are tough. 🥾👟👞🥿👠👡👢</p>
 
 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-  {#each data.product as product}
+  {#each product as product}
     <div class="bg-white shadow-md rounded-lg overflow-hidden">
-      {#each product.image as image}
-      <img src={supabase.storage.from('').getPublicUrl(image).data.publicUrl} alt={product.name} class="w-full h-48 object-cover" />
-      {/each}
+      <img src={supabase.storage.from('').getPublicUrl(product.image).data.publicUrl} alt={product.name} class="w-full h-48 object-cover" />
       <div class="p-4">
         <h3 class="text-2xl font-bold mb-2">{product.name}</h3>
         <p class="text-gray-700 mb-4">{product.description}</p>
